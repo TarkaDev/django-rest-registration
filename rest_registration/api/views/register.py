8d2c0bc9d489d07b7a48ff010a6bd51eaf4f4074
TarkaDev <contact@tdeconinck.com>
2018-09-17 19:25:36 +0200

    Set the profile as response when the user verify his address

--- rest_registration/api/views/register.py
+++ rest_registration/api/views/register.py
@@ -82,8 +82,12 @@ def verify_registration(request):
     '''
     Verify registration via signature.
     '''
-    process_verify_registration_data(request.data)
-    return get_ok_response('User verified successfully')
+    user = process_verify_registration_data(request.data)
+
+    output_serializer_class = registration_settings.REGISTER_OUTPUT_SERIALIZER_CLASS
+    output_serializer = output_serializer_class(instance=user)
+    user_data = output_serializer.data
+    return Response(user_data, status=200)
 
 
 def process_verify_registration_data(input_data):
@@ -100,3 +104,4 @@ def process_verify_registration_data(input_data):
     user = get_user_by_id(data['user_id'], require_verified=False)
     setattr(user, verification_flag_field, True)
     user.save()
+    return user
